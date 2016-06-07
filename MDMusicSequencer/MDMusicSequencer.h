30ccb80d9425ff10557de9c56ee0e7ce1dbd7489
Alex <alex@mac-mini-alex.local>
2016-06-07 15:56:49 +0600

    Initial Commit

---
+++ MDMusicSequencer/MDMusicSequencer.h
@@ -0,0 +1,19 @@
+//
+//  MDMusicSequencer.h
+//  MDMusicSequencer
+//
+//  Created by Alex on 07.06.16.
+//  Copyright © 2016 MakarovDmitry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MDMusicSequencer.
+FOUNDATION_EXPORT double MDMusicSequencerVersionNumber;
+
+//! Project version string for MDMusicSequencer.
+FOUNDATION_EXPORT const unsigned char MDMusicSequencerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDMusicSequencer/PublicHeader.h>
+
+
